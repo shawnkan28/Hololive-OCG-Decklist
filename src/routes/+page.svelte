05de6ad4a9ee5247ba5b9da20e66b41925a68fed
SKP-prog <shawn.personal28@outlook.com>
2026-08-10 19:29:50 +0800

    added cards. Need to do css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import ClickableDiv from '$lib/components/ClickableDiv.svelte';
 	import logo from '$lib/assets/favicon.png';
+	import { CARDS } from '$lib/data/card_data';
 </script>
 
 {@render navbar()}
 
-<div class="content"></div>
+<div class="content">
+	<div class="card-list">
+		{#each CARDS as card (card.rarity + ' ' + card.name + ' ' + card.number)}
+			{@render cardList(card)}
+		{/each}
+	</div>
+</div>
 
 {#snippet navbar()}
 	<div class="navbar">
@@ -22,5 +29,29 @@
 	</div>
 {/snippet}
 
+{#snippet cardList(card: {
+	url: string;
+	number: string;
+	name: string;
+	image: string;
+	priceJpy: number;
+	stockCount: number | null;
+})}
+	<ClickableDiv
+		class="card"
+		onclick={() => {
+			window.location.href = card.url;
+		}}
+	>
+		<img src={card.image} alt={card.number} />
+		<div class="card-details">
+			<div class="set-num">{card.number}</div>
+			<div class="card-name">{card.name}</div>
+		</div>
+		<div class="prices"><span>{card.priceJpy}</span><span>{card.stockCount} in stock</span></div>
+	</ClickableDiv>
+{/snippet}
+
 <style>
+	/* .content { } */
 </style>
