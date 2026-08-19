a7d264605399e0898e28b19ed3f97bd07a92a934
SKP-prog <shawn.personal28@outlook.com>
2026-08-19 20:28:23 +0800

    make dropdown list togglable

--- src/lib/components/FilterTalent.svelte
+++ src/lib/components/FilterTalent.svelte
@@ -2,31 +2,55 @@
 	import { TALENTS } from '$lib/data/card_data';
 	import ClickableDiv from './ClickableDiv.svelte';
 
+	let root: HTMLElement;
 	let isExpanded = $state(false);
+
+	function onDocumentClick(event: MouseEvent) {
+		// this means the event click and the item that was clicked is not the dropdown list, isExpanded will be closed
+		// and the event will still continue to trigger on the item that was clicked
+		// No preventDefault / stopPropogation so the click will still progress.
+		if (isExpanded && root && !root.contains(event.target as Node)) {
+			isExpanded = false;
+		}
+	}
 </script>
 
-<ClickableDiv
-	class="dropdownlist"
-	onclick={() => {
-		isExpanded = !isExpanded;
-	}}
->
-	<span>All talents</span>
+<!-- This is equivalent to document.addEventListener('click', onDocumentClick) -->
+<svelte:document onclick={onDocumentClick} />
+
+<div bind:this={root} class="dropdown-wrapper">
+	<ClickableDiv
+		class="dropdownlist"
+		onclick={() => {
+			isExpanded = !isExpanded;
+		}}
+	>
+		<span>All talents</span>
+		{#if isExpanded}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+				<!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+				<path
+					fill="currentColor"
+					d="M300.3 199.2C312.9 188.9 331.4 189.7 343.1 201.4L471.1 329.4C480.3 338.6 483 352.3 478 364.3C473 376.3 461.4 384 448.5 384L192.5 384C179.6 384 167.9 376.2 162.9 364.2C157.9 352.2 160.7 338.5 169.9 329.4L297.9 201.4L300.3 199.2z"
+				/>
+			</svg>
+		{:else}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+				<!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+				<path
+					fill="currentColor"
+					d="M300.3 440.8C312.9 451 331.4 450.3 343.1 438.6L471.1 310.6C480.3 301.4 483 287.7 478 275.7C473 263.7 461.4 256 448.5 256L192.5 256C179.6 256 167.9 263.8 162.9 275.8C157.9 287.8 160.7 301.5 169.9 310.6L297.9 438.6L300.3 440.8z"
+				/>
+			</svg>
+		{/if}
+	</ClickableDiv>
 	{#if isExpanded}
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-			<!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-			<path
-				fill="currentColor"
-				d="M300.3 199.2C312.9 188.9 331.4 189.7 343.1 201.4L471.1 329.4C480.3 338.6 483 352.3 478 364.3C473 376.3 461.4 384 448.5 384L192.5 384C179.6 384 167.9 376.2 162.9 364.2C157.9 352.2 160.7 338.5 169.9 329.4L297.9 201.4L300.3 199.2z"
-			/>
-		</svg>
-	{:else}
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-			<!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-			<path
-				fill="currentColor"
-				d="M300.3 440.8C312.9 451 331.4 450.3 343.1 438.6L471.1 310.6C480.3 301.4 483 287.7 478 275.7C473 263.7 461.4 256 448.5 256L192.5 256C179.6 256 167.9 263.8 162.9 275.8C157.9 287.8 160.7 301.5 169.9 310.6L297.9 438.6L300.3 440.8z"
-			/>
-		</svg>
+		<div class="dropdown-panel">
+			<div class="dropdown-panel-list">
+				{#each TALENTS as talent (talent.id)}
+					<div>{talent.en}</div>
+				{/each}
+			</div>
+		</div>
 	{/if}
-</ClickableDiv>
+</div>
